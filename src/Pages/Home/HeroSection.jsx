86f6b9f3a24d7931b9f22cc7f4bfafa8886e50c7
Raymond Yepiz <93514024+RayYepiz@users.noreply.github.com>
2024-07-01 19:10:08 -0700

    Worked on HeroSection, styles with CSS

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,16 +1,26 @@
+import heroImage from '../../images/heroImage.png'
+
 export default function HeroSection () {
     return (
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <p className="section--title"> I am Raymond</p>
+                    <p className="section--title"> Hey I am Raymond</p>
                     <h1 className="hero--section--title">
-                        <span className="hero--section--title--color">Full Stack</span>{" "}
+                        <span className="hero--section-title--color">Full Stack</span>{" "}
                         <br/>
+                        Developer
                     </h1>
-                    <p className="hero--section--description"> </p>
-                </div>
-            </div>
+                    <p className="hero--section--description">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br/>Lorem ipsum dolor, sit amet 
+                        consectetur adipisicing elit. Asperiores id, ratione aliquam ex qui perspiciatis. Praesentium.
+                    </p>
+                    </div>
+                    <button className='btn btn-primary'>Get In Touch</button>
+                    </div>
+                    <div className='hero--section--img'>
+                        <img src={heroImage} alt='Image of Raymond Yepiz merged with the letter R'/>
+                    </div>
         </section>
-    )
+    );
 }
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,5 +1,4 @@
 export default function Navbar(){
     return <>
-        Navigation bar
     </>
 }
